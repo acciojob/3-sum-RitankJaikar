af8a41d1b6e53629772eb9550394ab138162d9a9
Ritank Jaikar <ritank.jaikar@gmail.com>
2025-01-04 10:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function threeSum(arr, target) {
   //your code here
+  let closest;
+  let sum;
+  let minDiff = Infinity;
+  for(let i=0; i<arr.length; i++) {
+	for(let j=i+1; j<arr.length; j++) {
+	  for(let k=j+1; k<arr.length; k++) {
+		sum = arr[i]+arr[j]+arr[k];
+		if(Math.abs(sum-target)<minDiff) {
+			closest= sum;
+		}
+	  }
+	}
+  }
+
+  return closest;
 }
 
 module.exports = threeSum;
